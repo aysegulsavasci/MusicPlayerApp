d7349cced966e6f2a350902b01286fd112cd1a86
aysegulsavasci <aysegulsavasci56@gmail.com>
2025-01-21 15:39:47 +0300

    MusicPlayerApp

---
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.musicapp;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<AudioModel>  songslist = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        recyclerView  = findViewById(R.id.recycler_view);
+        noMusicTextView = findViewById(R.id.no_songs_text);
+
+        if (checkPermission() == false){
+            requestPermission();
+            return;
+        }
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+        String  selection = MediaStore.Audio.Media.IS_MUSIC +"!= 0";
+
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,projection,selection,null,null);
+        while (cursor.moveToNext()){
+            AudioModel songData = new AudioModel(cursor.getString(1), cursor.getString(0), cursor.getString(2) );
+            if (new File(songData.getPath()).exists())
+                songslist.add(songData);
+        }
+        if (songslist.size()==0){
+            noMusicTextView.setVisibility(View.VISIBLE);
+        }else{
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MusicListAdapter(songslist,getApplicationContext()));
+        }
+    }
+
+
+    boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (result  == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    @SuppressLint("SuspiciousIndentation")
+    void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this,"READ PERMISSION IS REQUIRED ALLOW FROM SETTINGS",Toast.LENGTH_SHORT).show();
+        }else
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (recyclerView!=null){
+            recyclerView.setAdapter(new MusicListAdapter(songslist,getApplicationContext()));
+        }
+    }
+}
